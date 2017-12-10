715003d06ba64d1f2ffe62a8db3c1b898726bedf
rinyPeng <3253421891@qq.com>
2017-12-10 13:43:06 +0800

    Add files via upload

---
+++ riny.storage.js
@@ -0,0 +1,62 @@
+var rinyStorage=function(option){
+	this.storage=option.storage;
+	this.limit=option.limit;
+	this.input=option.input;
+};
+rinyStorage.prototype.checkStorage=function(){
+	var storage=localStorage.getItem(this.storage);
+	if(!storage){
+		return;
+	};
+	this.history=JSON.parse(storage);
+	return this.history;
+};
+rinyStorage.prototype.setStorage=function(){
+
+	this.value=this.input.value;
+
+	this.checkStorage();
+	var history;
+	if(this.history){
+		history=this.history;
+	}else{
+		history=[];
+	};
+
+	var indexof=history.indexOf(this.value);
+
+	if(indexof!=-1){
+		history.splice(indexof,1);
+	};
+	history.unshift(this.value);
+
+	if(this.limit && history.length>this.limit){
+		history.splice(history.length-1,1);
+	};
+
+	history=JSON.stringify(history);
+	localStorage.setItem(this.storage,history);
+
+};
+rinyStorage.prototype.cancelStorage=function(val){
+
+	this.checkStorage();
+
+	if(!this.history) return;
+
+	if(!val){
+		localStorage.removeItem(this.storage);
+		this.history=undefined;
+	}else{
+		var indexof=this.history.indexOf(val+'');
+
+		if(indexof==-1){
+			return;
+		};
+
+		this.history.splice(indexof,1);
+		var history=JSON.stringify(this.history);
+		localStorage.setItem(this.storage,history);
+	};
+
+};
